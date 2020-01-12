ffaaec8ec9bd37eb57c0fc012116aaf90ea0c81c
Kirill <kirill.murawski@gmail.com>
2020-01-12 10:58:58 -0500

    Added user profile manager to handle user and superuser creations. Added new auth user model to the django settings file

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -121,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile' #Tells django to look for a specific model in a specific app
